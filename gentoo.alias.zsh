646e2b27d1b5aa86e9433ba6d6b61d008dff1ae2
littleblack111 <littleblack11111@gmail.com>
2025-02-27 18:47:54 +0800

    gentoo.alias: remove eepatch

--- gentoo.alias.zsh
+++ gentoo.alias.zsh
@@ -1,5 +1,4 @@
-function eppatch(){ /usr/bin/sudo mkdir -p /etc/portage/patches/$2; sudo wget $1.patch -d /etc/portage/patches/$2 }
-function epatch(){ /usr/bin/sudo mkdir -p /etc/portage/patches/$2; sudo wget $1 -d /etc/portage/patches/$2 }
+function epatch(){ /usr/bin/sudo mkdir -p /etc/portage/patches/$2; sudo wget $1 --allow-overwrite -d /etc/portage/patches/$2 }
 alias e='emerge'
 alias ee='emerge --noreplace'
 alias er='emerge --depclean'
